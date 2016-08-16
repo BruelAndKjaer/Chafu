b69b5ff05612474d957f645e62fc4a5cb5fefe06
Tomasz Cielecki <tomasz.cielecki@bksv.com>
2016-08-16 14:07:05 +0200

    Option for showing camera in Square mode

--- Fusuma/Configuration.cs
+++ Fusuma/Configuration.cs
@@ -7,12 +7,12 @@ namespace Fusuma
         public static UIColor BaseTintColor { get; set; } = UIColor.FromRGBA(255, 255, 255, 255);
         public static UIColor TintColor { get; set; } = UIColor.FromRGBA(0, 0x96, 0x88, 255);
         public static UIColor BackgroundColor { get; set; } = UIColor.FromRGBA(0x21, 0x21, 0x21, 255);
-        public static bool CropImage { get; set; } = false;
+        public static bool CropImage { get; set; } = true;
         public static bool TintIcons { get; set; } = true;
         public static string CameraRollTitle { get; set; } = "CAMERA ROLL";
         public static string CameraTitle { get; set; } = "PHOTO";
         public static string VideoTitle { get; set; } = "VIDEO";
-        public static ModeOrder ModeOrder { get; set; } = ModeOrder.LibraryFirst;
+        public static ModeOrder ModeOrder { get; set; } = ModeOrder.CameraFirst;
         public static UIImage FlashOnImage { get; set; }
         public static UIImage FlashOffImage { get; set; }
         public static UIImage FlipImage { get; set; }
@@ -26,5 +26,6 @@ namespace Fusuma
         public static UIImage CheckImage { get; set; }
         public static UIImage CloseImage { get; set; }
         public static bool PreferStatusbarHidden { get; set; } = true;
+        public static bool ShowSquare { get; set; } = true;
     }
 }
