52a18c5fc8d6052e6ffd68ba5d59a9c4358dcf75
Tomasz Cielecki <tomasz.cielecki@bksv.com>
2016-08-16 14:05:39 +0200

    Fix spacing when selecting image

--- Fusuma/PhotoGalleryDelegate.cs
+++ Fusuma/PhotoGalleryDelegate.cs
@@ -22,7 +22,8 @@ namespace Fusuma
             _albumView.ImageCropView.Scrollable = true;
             _albumView.ImageCropViewConstraintTop.Constant = AlbumView.ImageCropViewOriginalConstraintTop;
             _albumView.CollectionViewConstraintHeight.Constant = _albumView.Frame.Height -
-                AlbumView.ImageCropViewOriginalConstraintTop - _albumView.ImageCropViewContainer.Frame.Height;
+                AlbumView.ImageCropViewOriginalConstraintTop - _albumView.ImageCropView.Frame.Height;
+            _albumView.CollectionViewConstraintTop.Constant = 0;
 
             UIView.AnimateNotify(0.2, 0.0, UIViewAnimationOptions.CurveEaseOut, () => _albumView.LayoutIfNeeded(),
                 finished => { });
